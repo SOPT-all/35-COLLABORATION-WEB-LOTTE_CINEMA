3ced4925c8289508b99a4345c4e5b0224b023a08
goeun kim <rlarhdms6828@naver.com>
2024-11-18 23:40:03 +0900

    Chore: 페이지 생성

---
+++ Lotte-Cinema/src/pages/InfoCheck/InfoCheck.tsx
@@ -0,0 +1,5 @@
+const InfoCheck = () => {
+  return <h1>정보확인 및 인원 체크 페이지</h1>;
+};
+
+export default InfoCheck;
---
+++ Lotte-Cinema/src/pages/TheaterList/TheaterList.tsx
@@ -0,0 +1,5 @@
+const TheaterList = () => {
+  return <h1>상영관 선택 페이지</h1>;
+};
+
+export default TheaterList;
