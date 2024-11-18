0ffe54fc1136a23a7afb5fd6bc18ef9f2837e899
hanseo <yds06206@gmail.com>
2024-11-19 08:50:08 +0900

    Remove: 필요없는 코드 삭제

--- Lotte-Cinema/src/App.tsx
+++ Lotte-Cinema/src/App.tsx
@@ -1,12 +1,5 @@
 import router from "./routes";
-import styled from "@emotion/styled";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
-export const Main = styled.section`
-  display: flex;
-
-  color: white;
-`;
 
 const App = () => {
   const browserRouter = createBrowserRouter(router);
