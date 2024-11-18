89e2ae017172c4add2afc59c294595ba70f3d90e
constantly-dev <rntqhdl10@naver.com>
2024-11-18 19:18:39 +0900

    Init/#1: GlobalStyle 설정
    - reset.css를 사용한 Reset 파일 작성
    - 기본적인 스타일 초기화 GlobalStyle에 작성 (box-sizing: border-box 등)

--- Lotte-Cinema/src/main.tsx
+++ Lotte-Cinema/src/main.tsx
@@ -1,12 +1,14 @@
-import { ThemeProvider } from '@emotion/react';
+import { Global, ThemeProvider } from '@emotion/react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import GlobalStyle from './styles/global.ts';
 import theme from './styles/index.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
+      <Global styles={GlobalStyle} />
       <App />
     </ThemeProvider>
   </StrictMode>
---
+++ Lotte-Cinema/src/styles/global.ts
@@ -0,0 +1,35 @@
+import { css } from '@emotion/react';
+import Reset from './reset';
+
+const GlobalStyle = css`
+  ${Reset}
+
+  * {
+    box-sizing: border-box;
+  }
+
+  html,
+  body {
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    /* font-size: 62.5%; */
+    scrollbar-width: none;
+    scroll-behavior: smooth;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  select {
+    background: #fff;
+  }
+
+  /* Chrome, Safari 등에서 스크롤바 숨김 처리 */
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export default GlobalStyle;
