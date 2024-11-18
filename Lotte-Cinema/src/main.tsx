1c7a907b9e8ba69a572f026c809e16f081e8f339
hanseo <yds06206@gmail.com>
2024-11-18 20:05:04 +0900

    Init: axios 설치 및 탄스택 쿼리 설치

--- Lotte-Cinema/src/main.tsx
+++ Lotte-Cinema/src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { queryClient } from "@/hooks/queryClient.ts";
 
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={true} />
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
