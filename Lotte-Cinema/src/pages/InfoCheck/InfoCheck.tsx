ad1d5bfa21dc7e9aa585a89e12942bfa2a1a4d40
goeun kim <rlarhdms6828@naver.com>
2024-11-19 13:22:12 +0900

    Chore: 오타 수정

--- Lotte-Cinema/src/pages/InfoCheck/InfoCheck.tsx
+++ Lotte-Cinema/src/pages/InfoCheck/InfoCheck.tsx
@@ -4,7 +4,7 @@ import ModbileLayout from '@/components/mobileLayout/MobileLayout';
 const InfoCheck = () => {
 	return (
 		<ModbileLayout>
-			<Header title="인원 선택ㅇ" />
+			<Header title="인원 선택" />
 			<main>정보확인 및 인원 체크 페이지</main>
 		</ModbileLayout>
 	);
