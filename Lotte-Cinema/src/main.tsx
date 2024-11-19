abe1e274d223b5f1e4f63afa13373dd31761b8f2
hanseo <yds06206@gmail.com>
2024-11-19 09:13:09 +0900

    Remove: 필요없는 코드 삭제

--- Lotte-Cinema/src/main.tsx
+++ Lotte-Cinema/src/main.tsx
@@ -1,5 +1,4 @@
 import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "@/hooks/queryClient.ts";
 
 import { Global, ThemeProvider } from '@emotion/react';
