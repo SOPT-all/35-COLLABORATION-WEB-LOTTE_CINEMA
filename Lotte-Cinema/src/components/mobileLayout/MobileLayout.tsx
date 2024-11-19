140344c653bc3ebb4fb2994963479daeb1555486
goeun kim <rlarhdms6828@naver.com>
2024-11-20 00:10:53 +0900

    Refactor: 컴포넌트&  스타일 파일 통합

--- Lotte-Cinema/src/components/mobileLayout/MobileLayout.tsx
+++ Lotte-Cinema/src/components/mobileLayout/MobileLayout.tsx
@@ -1,16 +1,39 @@
-import S from "./MobileLayout.style";
-import { ReactNode } from "react";
+import styled from '@emotion/styled';
+
+import { ReactNode } from 'react';
 
 interface MobileLayoutProps {
-  children: ReactNode;
+	children: ReactNode;
 }
 
 const MobileLayout = ({ children }: MobileLayoutProps) => {
-  return (
-    <S.Wrapper role="main" aria-label="모바일 뷰">
-      <S.Layout>{children}</S.Layout>
-    </S.Wrapper>
-  );
+	return (
+		<S.Wrapper role="main" aria-label="모바일 뷰">
+			<S.Layout>{children}</S.Layout>
+		</S.Wrapper>
+	);
 };
 
 export default MobileLayout;
+
+const S = {
+	Wrapper: styled.div`
+		display: flex;
+		justify-content: center;
+		background-color: ${({ theme }) => theme.colors.GRAY03};
+	`,
+	Layout: styled.div`
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+
+		width: 360px;
+		min-height: 100vh;
+
+		background-color: ${({ theme }) => theme.colors.WHITE100};
+
+		& > main {
+			width: 100%;
+		}
+	`,
+};
