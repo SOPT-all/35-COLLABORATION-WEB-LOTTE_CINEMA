831b57f8910c6f0f2a62f4cd26548db6a718526e
luca <wjddhks1073@hufs.ac.kr>
2024-11-20 19:31:44 +0900

    Docs/#24: 사진 파일 추가

--- Lotte-Cinema/src/assets/svg/index.ts
+++ Lotte-Cinema/src/assets/svg/index.ts
@@ -1,2 +1,6 @@
-export { default as IcArrowLeftBlack24 } from './IcArrowLeftBlack24';
-export { default as IcHomeBlack24 } from './IcHomeBlack24';
+export { default as IcArrowLeftBlack24 } from "./IcArrowLeftBlack24";
+export { default as IcHomeBlack24 } from "./IcHomeBlack24";
+export { default as ImgPosterLargeAmazon } from "./ImgPosterLargeAmazon";
+export { default as ImgPosterLargeDeadline } from "./ImgPosterLargeDeadline";
+export { default as ImgPosterLargeEvent } from "./ImgPosterLargeEvent";
+export { default as ImgPosterLargeGladiator } from "./ImgPosterLargeGladiator";
