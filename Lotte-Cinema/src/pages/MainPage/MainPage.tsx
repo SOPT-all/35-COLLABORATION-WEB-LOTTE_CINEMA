a85a92112cc1063f203defe104e1f1e4bf995f61
luca <wjddhks1073@hufs.ac.kr>
2024-11-21 00:14:17 +0900

    Feat / #23 : 인기차트 영화 컴포넌트 mai에 추가

--- Lotte-Cinema/src/pages/MainPage/MainPage.tsx
+++ Lotte-Cinema/src/pages/MainPage/MainPage.tsx
@@ -1,11 +1,14 @@
-import MobileLayout from '@/components/mobileLayout/MobileLayout';
+import PopularMovieChart from "@/components/MainPageComponents/PopularMovieChart";
+import MobileLayout from "@/components/mobileLayout/MobileLayout";
 
 const MainPage = () => {
-	return (
-		<MobileLayout>
-			<main>메인페이지</main>
-		</MobileLayout>
-	);
+  return (
+    <MobileLayout>
+      <main>
+        <PopularMovieChart />
+      </main>
+    </MobileLayout>
+  );
 };
 
 export default MainPage;
