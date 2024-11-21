0679a7d75a37a349e549ea1cd75f6ee60e83b0b9
constantly-dev <rntqhdl10@naver.com>
2024-11-22 01:06:47 +0900

    Design/#25: 지역 List 컴포넌트 UI 구현

--- Lotte-Cinema/src/pages/TheaterList/TheaterList.tsx
+++ Lotte-Cinema/src/pages/TheaterList/TheaterList.tsx
@@ -1,3 +1,4 @@
+import List from '@/components/TheaterList/List';
 import TabBar from '@/components/TheaterList/TabBar';
 import Header from '@/components/commons/header/Header';
 import MobileLayout from '@/components/mobileLayout/MobileLayout';
@@ -7,6 +8,7 @@ const TheaterList = () => {
 		<MobileLayout>
 			<Header title="영화관 선택" />
 			<TabBar />
+			<List />
 		</MobileLayout>
 	);
 };
