49c051abb49c9670621f469fed7712577aa55960
goeun kim <rlarhdms6828@naver.com>
2024-11-21 14:22:32 +0900

    Chore: 연령 아이콘/ 화살표아이콘 svgr 생성

--- Lotte-Cinema/src/assets/svg/index.ts
+++ Lotte-Cinema/src/assets/svg/index.ts
@@ -1,2 +1,6 @@
+export { default as IcAge1216 } from './IcAge1216';
+export { default as IcAge1916 } from './IcAge1916';
+export { default as IcAgeAll16 } from './IcAgeAll16';
 export { default as IcArrowLeftBlack24 } from './IcArrowLeftBlack24';
+export { default as IcArrowRightWhite10 } from './IcArrowRightWhite10';
 export { default as IcHomeBlack24 } from './IcHomeBlack24';
