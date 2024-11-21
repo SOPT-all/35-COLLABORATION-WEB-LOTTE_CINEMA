1ec46b7462f938240f4ca9bda5348596cee2defb
hanseo <yds06206@gmail.com>
2024-11-21 23:31:39 +0900

    feat: 예매완료 모달 구현

--- Lotte-Cinema/src/assets/svg/index.ts
+++ Lotte-Cinema/src/assets/svg/index.ts
@@ -1,3 +1,4 @@
 export { default as IcArrowLeftBlack24 } from './IcArrowLeftBlack24';
 export { default as IcHomeBlack24 } from './IcHomeBlack24';
 export { default as IcLpayLogo } from './IcLpayLogo';
+export { default as IcPaymentComplete } from './IcPaymentComplete';
