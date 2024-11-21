e2be9bcb6036e98590e84e25cb1c95ad9c9dc329
luca <wjddhks1073@hufs.ac.kr>
2024-11-21 11:03:52 +0900

    Docs / #27 : 이미지 파일(로고, 티켓 사진) 추가

--- Lotte-Cinema/src/assets/svg/index.ts
+++ Lotte-Cinema/src/assets/svg/index.ts
@@ -1,2 +1,4 @@
-export { default as IcArrowLeftBlack24 } from './IcArrowLeftBlack24';
-export { default as IcHomeBlack24 } from './IcHomeBlack24';
+export { default as IcArrowLeftBlack24 } from './IcArrowLeftBlack24'
+export { default as IcHomeBlack24 } from './IcHomeBlack24'
+export { default as IcTicketBlack32 } from './IcTicketBlack32'
+export { default as ImgLogo } from './ImgLogo'
