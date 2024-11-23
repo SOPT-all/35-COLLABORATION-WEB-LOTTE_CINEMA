6c40c5443c68aa671f7711dc6e6fb87e638c09f8
luca <wjddhks1073@hufs.ac.kr>
2024-11-23 19:47:33 +0900

    Design / #30 : 중앙 menu 원 - 아이콘 - 텍스트 구조로 변경하기(수정 필요)

--- Lotte-Cinema/src/components/mainPage/NavFooter.tsx
+++ Lotte-Cinema/src/components/mainPage/NavFooter.tsx
@@ -1,6 +1,9 @@
 import styled from '@emotion/styled';
 
-import { BtnCircle, IcEventBlack24, IcMenuBlack24, IcMyBlack24, IcTimeBlack24 } from '@/assets/svg';
+import { IcEventBlack24, IcMenuBlack24, IcMyBlack24, IcTicketWhite24, IcTimeBlack24, ImgCircle56 } from '@/assets/svg';
+
+// FooterNav 부분에서 바로 예매 파트 수정하기, -> 아이콘과 텍스트 원 위에서 세로축, 가로축 정렬되게 하기
+// -> margin-bottom: 0.8rem 적용 안되는 문제 해결하기
 
 // Menu 순서 IcEventBlack24 / IcTimeBlack24 / BtnCircle / IcMyBlack24 / IcMenuBlack24
 const NavFooter = () => {
@@ -16,8 +19,15 @@ const NavFooter = () => {
 			</S.MenuBox>
 			<S.MenuBox>
 				<S.MenuCenter>
-					{/* 그냥 이것도 텍스트 + 아이콘 + 원 조합으로 바꾸기 */}
-					<BtnCircle />
+					<S.ImgCircleContainer>
+						<ImgCircle56 />
+					</S.ImgCircleContainer>
+					<S.MenuCenterContentContainer>
+						<S.IconAndTextContainer>
+							<IcTicketWhite24 height="3.4rem" />
+							<S.MenuCenterText>바로예매</S.MenuCenterText>
+						</S.IconAndTextContainer>
+					</S.MenuCenterContentContainer>
 				</S.MenuCenter>
 			</S.MenuBox>
 			<S.MenuBox>
@@ -53,9 +63,40 @@ const S = {
 	MenuCenter: styled.div`
 		width: 100%;
 		height: 100%;
+		position: relative;
 	`,
 
 	MenuText: styled.li`
 		list-style: none;
+	`,
+
+	ImgCircleContainer: styled.div``,
+	MenuCenterContentContainer: styled.div`
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		color: ${({ theme }) => theme.colors.WHITE100};
+	`,
+	IconAndTextContainer: styled.div`
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-start;
+		gap: 1rem;
+	`,
+	StyledImgCircle56: styled(ImgCircle56)`
+		width: 100%;
+		position: absolute;
+		z-index: -1;
+	`,
+	StyledIcTicketWhite24: styled(IcTicketWhite24)`
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		z-index: 2;
+	`,
+	MenuCenterText: styled.span`
+		position: absolute;
 	`,
 };
