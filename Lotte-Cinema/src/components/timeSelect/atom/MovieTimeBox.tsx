ddead48dc5ebe1952c1ba1857420bf9dd2a255ed
goeun kim <rlarhdms6828@naver.com>
2024-11-25 14:46:44 +0900

    Feat: 상영 시간 아코디언 형태

--- Lotte-Cinema/src/components/timeSelect/atom/MovieTimeBox.tsx
+++ Lotte-Cinema/src/components/timeSelect/atom/MovieTimeBox.tsx
@@ -1,36 +1,12 @@
 import styled from '@emotion/styled';
 
-import { IcArrowTop20 } from '@/assets/svg';
-
-import theme from '@/styles';
-
-import TimeCard from './TimeCard';
+import TimeAccordian from './TimeAccordian';
 
 const MovieTimeBox = () => {
   return (
     <S.Wrapper>
-      <S.TitleHeader>
-        <S.TitleContainer>
-          <h1>건대 입구</h1>
-          <IcArrowTop20 width="2rem" />
-        </S.TitleContainer>
-      </S.TitleHeader>
-      <S.InfoAccordion>
-        <S.InfoContainer>
-          <S.InfoTrigger>
-            <p>4관 2D</p>
-            <span />
-            <p>리클라이너</p>
-          </S.InfoTrigger>
-          <S.InfoContent>
-            <TimeCard />
-            <TimeCard />
-            <TimeCard />
-            <TimeCard />
-            <TimeCard />
-          </S.InfoContent>
-        </S.InfoContainer>
-      </S.InfoAccordion>
+      <TimeAccordian />
+      <TimeAccordian />
     </S.Wrapper>
   );
 };
@@ -47,67 +23,5 @@ const S = {
     height: 3.37rem;
     flex-direction: column;
     align-items: center;
-  `,
-
-  TitleHeader: styled.header`
-    display: flex;
-    height: 5.6rem;
-    padding: 1.8rem;
-    flex-direction: column;
-    align-items: flex-start;
-    gap: 1rem;
-    align-self: stretch;
-  `,
-
-  TitleContainer: styled.div`
-    display: flex;
-    width: 100%;
-    justify-content: space-between;
-    align-items: center;
-
-    & h1 {
-      ${theme.typographies.n_head02_bold}
-    }
-  `,
-
-  InfoAccordion: styled.section`
-    display: flex;
-    width: 100%;
-    flex-direction: column;
-    align-items: center;
-  `,
-
-  InfoContainer: styled.section`
-    width: 100%;
-    padding: 0 1.8rem;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    gap: 1rem;
-    margin-bottom: 1.8rem;
-  `,
-
-  InfoTrigger: styled.header`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    gap: 0.6rem;
-
-    & p {
-      ${({ theme }) => theme.typographies.n_body02_reg}
-    }
-
-    & span {
-      background-color: ${({ theme }) => theme.colors.GRAY04};
-      width: 0.1rem;
-      height: 0.9rem;
-    }
-  `,
-
-  InfoContent: styled.div`
-    width: 100%;
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    gap: 0.8rem;
   `,
 };
---
+++ Lotte-Cinema/src/components/timeSelect/atom/TimeAccordian.tsx
@@ -0,0 +1,119 @@
+import styled from '@emotion/styled';
+
+import { useState } from 'react';
+
+import { IcArrowDown20, IcArrowTop20 } from '@/assets/svg';
+
+import TimeCard from './TimeCard';
+
+const TimeAccordian = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(true);
+
+  const toggleAccordion = () => {
+    setIsOpen((prev) => !prev);
+  };
+  return (
+    <>
+      <S.TitleHeader onClick={toggleAccordion}>
+        <S.TitleContainer $isopen={isOpen}>
+          <h1>건대 입구</h1>
+          {isOpen ? <IcArrowTop20 width="2rem" /> : <IcArrowDown20 width="2rem" />}
+        </S.TitleContainer>
+      </S.TitleHeader>
+      {isOpen && (
+        <S.InfoAccordion $isopen={isOpen}>
+          <S.InfoContainer>
+            <S.InfoTitle>
+              <p>4관 2D</p>
+              <span />
+              <p>리클라이너</p>
+            </S.InfoTitle>
+            <S.InfoContent>
+              <TimeCard />
+              <TimeCard />
+              <TimeCard />
+              <TimeCard />
+              <TimeCard />
+            </S.InfoContent>
+          </S.InfoContainer>
+        </S.InfoAccordion>
+      )}
+      <S.ShadowLine />
+    </>
+  );
+};
+
+export default TimeAccordian;
+
+const S = {
+  TitleHeader: styled.header`
+    display: flex;
+    height: 5.6rem;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 1rem;
+    align-self: stretch;
+  `,
+
+  TitleContainer: styled.div<{ $isopen: boolean }>`
+    display: flex;
+    width: 100%;
+    padding: 1.8rem;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: ${({ $isopen, theme }) => (!$isopen ? `0.6rem solid ${theme.colors.GRAY02}` : `none`)};
+    & h1 {
+      ${({ theme }) => theme.typographies.n_head02_bold}
+    }
+  `,
+
+  InfoAccordion: styled.section<{ $isopen: boolean }>`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+
+  border-bottom : ${({ $isopen, theme }) => ($isopen ? `0.6rem solid ${theme.colors.GRAY02}` : `none`)}
+  }
+`,
+
+  InfoContainer: styled.section`
+    width: 100%;
+    padding: 0 1.8rem;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 1rem;
+    margin-bottom: 1.8rem;
+  `,
+
+  InfoTitle: styled.header`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 0.6rem;
+
+    & p {
+      ${({ theme }) => theme.typographies.n_body02_reg}
+    }
+
+    & span {
+      background-color: ${({ theme }) => theme.colors.GRAY04};
+      width: 0.1rem;
+      height: 0.9rem;
+    }
+  `,
+
+  InfoContent: styled.div`
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 0.8rem;
+  `,
+
+  ShadowLine: styled.span`
+    width: 100%;
+    height: 0.6rem;
+    background-color: ${({ theme }) => theme.colors.GRAY02};
+  `,
+};
