c87e2719738c774cf9cde538c01d500379dc4f27
luca <wjddhks1073@hufs.ac.kr>
2024-11-25 15:12:30 +0900

    Style / #23 : 이미지 파일 불필요한 코드 삭제

--- Lotte-Cinema/src/assets/svg/IcStarGray1010.tsx
+++ Lotte-Cinema/src/assets/svg/IcStarGray1010.tsx
@@ -1,3 +1,11 @@
-import type { SVGProps } from "react";
-const SvgIcStarGray1010 = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 10" {...props}><path fill="#6D6D6D" d="m5 7.196 1.73 1.046a.416.416 0 0 0 .62-.45l-.458-1.967L8.42 4.5c.279-.242.129-.7-.238-.73L6.171 3.6l-.788-1.858a.417.417 0 0 0-.766 0l-.788 1.854-2.012.17a.416.416 0 0 0-.238.73L3.11 5.82 2.65 7.788a.416.416 0 0 0 .62.45z" /></svg>;
+import type { SVGProps } from 'react';
+
+const SvgIcStarGray1010 = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 10" {...props}>
+    <path
+      fill="#6D6D6D"
+      d="m5 7.196 1.73 1.046a.416.416 0 0 0 .62-.45l-.458-1.967L8.42 4.5c.279-.242.129-.7-.238-.73L6.171 3.6l-.788-1.858a.417.417 0 0 0-.766 0l-.788 1.854-2.012.17a.416.416 0 0 0-.238.73L3.11 5.82 2.65 7.788a.416.416 0 0 0 .62.45z"
+    />
+  </svg>
+);
 export default SvgIcStarGray1010;
