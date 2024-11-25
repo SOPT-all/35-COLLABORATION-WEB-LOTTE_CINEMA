9065c1ebaf158f7537e2c11c7f44c8b656c92a08
goeun kim <rlarhdms6828@naver.com>
2024-11-25 16:10:42 +0900

    Style: never used 라인 삭제

--- Lotte-Cinema/src/assets/svg/IcArrowTop20.tsx
+++ Lotte-Cinema/src/assets/svg/IcArrowTop20.tsx
@@ -1,4 +1,11 @@
-import * as React from "react";
-import type { SVGProps } from "react";
-const SvgIcArrowTop20 = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" {...props}><path fill="#848484" d="m15.02 13 .98-1.041-5.345-5.67a.9.9 0 0 0-.3-.214.88.88 0 0 0-1.007.213L4 11.958 4.98 13 10 7.677z" /></svg>;
+import type { SVGProps } from 'react';
+
+const SvgIcArrowTop20 = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" {...props}>
+    <path
+      fill="#848484"
+      d="m15.02 13 .98-1.041-5.345-5.67a.9.9 0 0 0-.3-.214.88.88 0 0 0-1.007.213L4 11.958 4.98 13 10 7.677z"
+    />
+  </svg>
+);
 export default SvgIcArrowTop20;
