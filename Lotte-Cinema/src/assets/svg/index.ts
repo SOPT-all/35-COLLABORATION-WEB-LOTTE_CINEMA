e44a344fd9fe11f990dbc92a41957704edecb635
hanseo <yds06206@gmail.com>
2024-11-25 18:19:35 +0900

    feat: 좌석표 ui 구현

--- Lotte-Cinema/src/assets/svg/index.ts
+++ Lotte-Cinema/src/assets/svg/index.ts
@@ -7,6 +7,8 @@ export { default as IcAgeAll16 } from './IcAgeAll16';
 export { default as IcArrowLeftBlack24 } from './IcArrowLeftBlack24';
 export { default as IcArrowRightWhite10 } from './IcArrowRightWhite10';
 export { default as IcCloseGray1414 } from './IcCloseGray1414';
+
+export { default as IcCloseGrayTemp } from './IcCloseGrayTemp';
 export { default as IcEntrance10 } from './IcEntrance10';
 export { default as IcHomeBlack24 } from './IcHomeBlack24';
 export { default as IcSeatDisabled10 } from './IcSeatDisabled10';
@@ -16,5 +18,3 @@ export { default as ImgLogo } from './ImgLogo';
 export { default as ImgLpayLogo } from './ImgLpayLogo';
 export { default as ImgPaymentComplete } from './ImgPaymentComplete';
 export { default as ImgPosterSmallAmazonhms } from './ImgPosterSmallAmazonhms';
-
-export { default as IcCloseGrayTemp } from './IcCloseGrayTemp';
