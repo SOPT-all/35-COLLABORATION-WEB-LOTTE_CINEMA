03122557b9239d7862819ac297e9106e49c8c3a0
luca <wjddhks1073@hufs.ac.kr>
2024-11-26 15:26:38 +0900

    Style / #23 : 포매팅

--- Lotte-Cinema/src/pages/MainPage/MainPage.tsx
+++ Lotte-Cinema/src/pages/MainPage/MainPage.tsx
@@ -1,18 +1,16 @@
-import MainPageHeader from '@/components/mainPage/MainPageHeader';
-import MobileLayout from '@/components/mobileLayout/MobileLayout';
-import PopularMovieChart from '@/components/mainPage/PopularMovieChart';
 import MainImage from '@/components/mainPage/MainImage';
+import MainPageHeader from '@/components/mainPage/MainPageHeader';
+import PopularMovieChart from '@/components/mainPage/PopularMovieChart';
+import MobileLayout from '@/components/mobileLayout/MobileLayout';
 
 const MainPage = () => {
-
-	return (
-		<MobileLayout>
-			<MainPageHeader />
-			<MainImage />
-      		<PopularMovieChart/>
-		</MobileLayout>
-	);
-
+  return (
+    <MobileLayout>
+      <MainPageHeader />
+      <MainImage />
+      <PopularMovieChart />
+    </MobileLayout>
+  );
 };
 
 export default MainPage;
