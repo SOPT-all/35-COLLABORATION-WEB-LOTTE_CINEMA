bd13faa4c8fe912f57edb7db11123e0212a8149c
hanseo <yds06206@gmail.com>
2024-11-30 07:06:56 +0900

    feat: 미니맵 좌석 구현

--- Lotte-Cinema/src/components/seatReservation/MiniMap.tsx
+++ Lotte-Cinema/src/components/seatReservation/MiniMap.tsx
@@ -2,6 +2,10 @@ import styled from '@emotion/styled';
 
 import { RefObject } from 'react';
 
+import { IcSeatDefaultSmall } from '@/assets/svg';
+
+import { SEAT_INFO, SEAT_ROWS } from '@/constants';
+
 interface MiniMapProps {
   selectedSeats: string[];
   miniMapRef: RefObject<HTMLDivElement>;
@@ -9,10 +13,34 @@ interface MiniMapProps {
   viewport: { left: number; width: number };
 }
 
-const MiniMap = ({ miniMapViewportRef, viewport, miniMapRef }: MiniMapProps) => {
+const MiniMap = ({ miniMapViewportRef, viewport, miniMapRef, selectedSeats }: MiniMapProps) => {
   return (
     <S.MiniMapWrapper ref={miniMapRef}>
       <S.ScreenWrapper>Screen</S.ScreenWrapper>
+      <S.SeatTableBody>
+        {SEAT_ROWS.map((row) => (
+          <S.SeatRowWrapper key={row}>
+            {SEAT_INFO.filter((seat) => seat.startsWith(row)).map((seat) => {
+              const isSelected = selectedSeats.includes(seat);
+
+              const marginRight = [2, 11].includes(parseInt(seat.slice(1))) ? '0.7rem' : '0';
+              const commonProps = {
+                width: '0.4rem',
+                style: { marginRight },
+              };
+
+              return (
+                <IcSeatDefaultSmall
+                  key={seat}
+                  fill={isSelected ? '#FF243E' : '#1EAFFD'}
+                  {...commonProps}
+                  style={{ ...commonProps.style, cursor: 'pointer' }}
+                />
+              );
+            })}
+          </S.SeatRowWrapper>
+        ))}
+      </S.SeatTableBody>
       <div
         ref={miniMapViewportRef}
         style={{
@@ -48,5 +76,14 @@ const S = {
     ${({ theme }) => theme.typographies.n_caption01_reg};
     color: ${({ theme }) => theme.colors.GRAY12};
     border-bottom: 1px solid ${({ theme }) => theme.colors.GRAY12};
+    margin-bottom: 0.9rem;
+  `,
+  SeatTableBody: styled.div`
+    padding: 0rem 0rem;
+  `,
+  SeatRowWrapper: styled.div`
+    display: flex;
+    column-gap: 0.1rem;
+    margin-bottom: 0.1rem;
   `,
 };
